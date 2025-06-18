50cc5601a479608bcc56e9fbe38df79fa2f81f49
LadLabs <94710371+ladsec@users.noreply.github.com>
2025-06-18 20:11:13 +0200

    onedrive_migrace
    zaloha nastaveni onedrive a sharepointu pro migraci na jiny pc

---
+++ onedrive_migrace.ps1
@@ -0,0 +1,38 @@
+﻿$adminUser = Read-Host "Zadej admin uzivatelske jmeno"
+$adminPass = Read-Host "Zadej admin heslo" -AsSecureString
+$remotePC = Read-Host "Zadej nazev nebo IP vzdalenyho PC"
+
+# Vytvoreni PS credential objektu
+$cred = New-Object System.Management.Automation.PSCredential ($adminUser, $adminPass)
+
+# Spusteni skriptu na vzdalenem PC
+Invoke-Command -ComputerName $remotePC -Credential $cred -ScriptBlock {
+
+    # ukonceni procesu
+    Write-Host "ukoncovani procesu onedrive"
+    $oneDriveProcess = Get-Process -Name OneDrive -ErrorAction SilentlyContinue
+    if ($oneDriveProcess) {
+        Stop-Process -Name OneDrive -Force -ErrorAction SilentlyContinue
+    }
+
+    # ziskani uziv uctu ez slozky c:\users
+    Write-Host "ziskani uzivatelskych uctu z c:\users"
+    $users = Get-ChildItem -Path "C:\Users" -Directory
+
+    # zaloha registru pro onedrive vsech uziv
+    foreach ($user in $users) {
+        $userHive = "C:\Users\$($user.Name)\NTUSER.DAT" # inicializace $userHive
+        try {
+            reg load HKU\TempHive "$userHive"
+            Write-Host "Registr $userHive byl úspěšně načten."
+            reg export HKU\TempHive\Software\Microsoft\OneDrive "C:\Users\$($user.Name)\OneDrive_Backup.reg" /y
+            Write-Host "Registr OneDrive byl úspěšně exportován pro uživatele $($user.Name)."
+            reg unload HKU\TempHive
+            Write-Host "Registr HKU\TempHive byl úspěšně uvolněn."
+        } catch {
+            Write-Error "Chyba při zálohování registru pro uživatele $($user.Name): $_"
+        }
+    }
+
+    Write-Host "OneDrive byl zalohovan"
+}
